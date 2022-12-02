54d99ff2175df6c7b1c8b37e732f54d5818a48f4
C Martin <cmart131@yahoo.com>
2022-12-01 16:53:33 -0800

    added css to asset folder, added code to HTML, and JS files. added reset css file

---
+++ script.js
@@ -0,0 +1,45 @@
+var questions = [{
+    question: "1. How do you write 'Hello World' in an alert box?",
+    choices: ["msg('Hello World')", "msgBox('Hello World');", "alertBox('Hello World');", "alert('Hello World');"],
+    correctAnswer: 3
+}, {
+    question: "2. ____ is the process of finding errors and fixing them within a program.",
+    choices: ["Executing", "Debugging", "Scanning", "Compiling"],
+    correctAnswer: 1
+}, {
+    question: "3. Which of the sets of statements below will add 1 to x if x is positive and subtract 1 from x if x is negative but leave x alone if x is 0?",
+    choices: ["If (x > 0) x++; else x--;", "X++; x--;", "If (x > 0) x++; else if (x < 0) x--;", "If (x == 0) x = 0; else x++; x--;"],
+    correctAnswer: 3
+}, {
+    question: "4. What will this output? var a = [1, 2, 3]; console.log(a[6]);",
+    choices: ["undefined", "0", "prints nothing", "Syntax error"],
+    correctAnswer: 0
+}, {
+    question: "5. What would following code return? console.log(typeof typeof 1);",
+    choices: ["string", "number", "Syntax error", "undefined"],
+    correctAnswer: 0
+},{
+	question: "6. Which software company developed JavaScript?",
+    choices: ["Mozilla", "Netscape", "Sun Microsystems", "Oracle"],
+    correctAnswer: 1
+},{
+	question: "7. Kim has just constructed her first for loop within the Java language.  Which of the following is not a required part of a for loop?",
+    choices: ["Initialization", "Variable", "increment", "Condition"],
+    correctAnswer: 1
+},{
+	question: "8. Look at the following selector: $('div'). What does it select?",
+    choices: ["The first div element", "The last div element", "All div elements", "Current div element"],
+    correctAnswer: 2
+},{
+	question: "9. Which command will stop an infinite loop?",
+    choices: ["Alt - C", "Shift - C", "Esc", "Ctrl - C"],
+    correctAnswer: 3
+},{
+	question: "10. Sal needs to execute a section of code ten times within a program. Compare the selection structures below and select which one meets the needs identified.",
+    choices: ["If-Else", "For", "While", "If"],
+    correctAnswer: 1
+}];
+
+
+
+
