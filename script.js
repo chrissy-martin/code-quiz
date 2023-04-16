1f48c84444de4f127b3b70f70f3fa98433344286
C Martin <cmart131@yahoo.com>
2023-04-16 15:18:48 -0700

    fixed code and styling

--- script.js
+++ script.js
@@ -32,44 +32,51 @@ var questions = [
 }];
 
 
-// Welcome Page Elements =====================================
-var welcomeEl = document.querySelector("#welcome");
-var startQuizBtnEl = document.querySelector("#startQuiz");
-
-//Quiz Page Elements =========================================
-var quizEl = document.querySelector("#quiz");
-var questionEl = document.querySelector("#ask_question");
-var answersEl = document.querySelector("#answers");
+var welcome = document.querySelector("#welcome");
+var start = document.querySelector("#startQuiz");
+var quiz = document.querySelector("#quiz");
+var askQuestion = document.querySelector("#ask_question");
+var answers = document.querySelector("#answers");
 var checkLine = document.querySelector("#check_line");
-
-//Input Score Page Elements ==================================
-var inputScoreEl = document.querySelector("#inputScore");
-var initialsEl = document.querySelector("#initials");
-var submitInitialsBtnEl = document.querySelector("#submitInitials");
-var userScoreEl = document.querySelector("#score");
-
-//View High Scores Page Elements =============================
-var highScoresEl = document.querySelector("#highScores");
-var scoresEl = document.querySelector("#scores");
-var goBackBtnEl = document.querySelector("#goBack");
-var clearScoresBtnEl = document.querySelector("#clearScores");
-
-//Universal vars =============================================
-var viewHScoresBtnEl = document.querySelector("#viewHScores");
-var timerEl = document.querySelector("#timer");
+var inputScore = document.querySelector("#inputScore");
+var initials = document.querySelector("#initials");
+var submit = document.querySelector("#submitInitials");
+var userScore = document.querySelector("#score");
+var hScore = document.querySelector("#highScores");
+var allScores = document.querySelector("#scores");
+var goBack = document.querySelector("#goBack");
+var clear = document.querySelector("#clearScores");
+var viewScores = document.querySelector("#viewHScores");
+var timer = document.querySelector("#timer");
+const lastQuestion = questions.length - 1;
 var score = 0;
 var currentQ = 0;
 var highScores = [];
-var interval;
+var interval = 0;
 var timeGiven = 75;
 var secondsElapsed = 0;
 
-//starts and updates timer
+
+start.addEventListener("click", function () {
+    hide(welcome);
+    startTimer();
+    renderQuestion();
+    show(quiz);
+});
+
+
+answers.addEventListener("click", function (e) {
+    if (e.target.matches("button")) {
+        checkAnswer(e.target);
+        nextQuestion();
+    }
+});
+
 function startTimer() {
-    timerEl.textContent = timeGiven;
+    timer.textContent = timeGiven;
     interval = setInterval(function () {
         secondsElapsed++;
-        timerEl.textContent = timeGiven - secondsElapsed;
+        timer.textContent = timeGiven - secondsElapsed;
         if (secondsElapsed >= timeGiven) {
             currentQ = questions.length;
             nextQuestion();
@@ -77,13 +84,17 @@ function startTimer() {
     }, 1000);
 }
 
-//stops timer
 function stopTimer() {
     clearInterval(interval);
 }
 
-//Clears current question and calls for display of next question
-//Calls for input score display if last question
+function renderQuestion() {
+    askQuestion.textContent = questions[currentQ].question;
+    for (i = 0; i < answers.children.length; i++) {
+        answers.children[i].children[0].textContent = `${(i + 1)}: ${questions[currentQ].choices[i]}`;
+    }
+}
+
 function nextQuestion() {
     currentQ++;
     if (currentQ < questions.length) {
@@ -92,14 +103,13 @@ function nextQuestion() {
         stopTimer();
         if ((timeGiven - secondsElapsed) > 0)
             score += (timeGiven - secondsElapsed);
-        userScoreEl.textContent = score;
-        hide(quizEl);
-        show(inputScoreEl);
-        timerEl.textContent = 0;
+        userScore.textContent = score;
+        hide(quiz);
+        show(inputScore);
+        timer.textContent = 0;
     }
 }
 
-//checks answer based on current question and updates the user score
 function checkAnswer(answer) {
     if (questions[currentQ].answer == questions[currentQ].choices[answer]) {
         score += 5;
@@ -111,7 +121,6 @@ function checkAnswer(answer) {
     }
 }
 
-//displays a message for 2 seconds
 function displayMessage(m) {
     var messageHr = document.createElement("hr");
     var messageEl = document.createElement("div");
@@ -125,39 +134,27 @@ function displayMessage(m) {
 
 }
 
-//hides element
 function hide(element) {
     element.style.display = "none";
 }
 
-//displays element
+
 function show(element) {
     element.style.display = "block";
 }
 
-//reset local variables
+
 function reset() {
     score = 0;
     currentQ = 0;
     secondsElapsed = 0;
-    timerEl.textContent = 0;
+    timer.textContent = 0;
 }
 
-//=================== Rendering ================================
 
-//Renders current question
-function renderQuestion() {
-    questionEl.textContent = questions[currentQ].question;
-    for (i = 0; i < answersEl.children.length; i++) {
-        answersEl.children[i].children[0].textContent = `${(i + 1)}: ${questions[currentQ].choices[i]}`;
-    }
-}
-
-//Renders high scores stored in local storage
 function renderHighScores() {
-    // Clear content
-    scoresEl.innerHTML = "";
-    show(highScoresEl);
+    allScores.innerHTML = "";
+    show(hScore);
     highScores = JSON.parse(localStorage.getItem("scores"));
     for (var i = 0; i < highScores.length; i++) {
         var scoreItem = document.createElement("div");
@@ -165,68 +162,41 @@ function renderHighScores() {
         console.log(scoreItem)
         scoreItem.setAttribute("style", "background-color:PaleTurquoise;");
         scoreItem.textContent = `${(i + 1)}. ${highScores[i].username} - ${highScores[i].userScore}`;
-        scoresEl.appendChild(scoreItem);
+        allScores.appendChild(scoreItem);
     }
 }
 
-
-//=========================EVENTS================================
-
-//displays high scores
-viewHScoresBtnEl.addEventListener("click", function () {
-    hide(welcomeEl);
-    hide(quizEl);
-    hide(inputScoreEl);
+viewScores.addEventListener("click", function () {
+    hide(welcome);
+    hide(quiz);
+    hide(inputScore);
     renderHighScores();
     stopTimer();
     reset();
 });
 
-//starts quiz from  Welcome page
-startQuizBtnEl.addEventListener("click", function () {
-    hide(welcomeEl);
-    startTimer();
-    renderQuestion();
-    show(quizEl);
-});
 
-//Calls to check answer selected and calls to next question if button is clicked
-answersEl.addEventListener("click", function (e) {
-    if (e.target.matches("button")) {
-        checkAnswer(e.target);
-        nextQuestion();
-    }
-});
-
-//Creates a user score object to push to the local storage scores array calls to display high scores
-//calls to render high scores
-submitInitialsBtnEl.addEventListener("click", function () {
-    var initValue = initialsEl.value.trim();
+submit.addEventListener("click", function () {
+    var initValue = initials.value.trim();
     if (initValue) {
         var userScore = { username: initValue, userScore: score };
-        initialsEl.value = '';
+        initials.value = '';
         highScores = JSON.parse(localStorage.getItem("scores")) || [];
         highScores.push(userScore)
         localStorage.setItem("scores", JSON.stringify(highScores));
-        hide(inputScoreEl);
+        hide(inputScore);
         renderHighScores();
         reset();
     }
 });
 
-//Goes back to Welcome page from High scores 
-goBackBtnEl.addEventListener("click", function () {
-    hide(highScoresEl);
-    show(welcomeEl);
+goBack.addEventListener("click", function () {
+    hide(hScore);
+    show(welcome);
 });
 
-//Clears saved scores from local storage
-clearScoresBtnEl.addEventListener("click", function () {
+clear.addEventListener("click", function () {
     highScores = [];
     localStorage.setItem("scores", JSON.stringify(highScores));
     renderHighScores();
 }); 
-
-
-
-
